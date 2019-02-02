803f68fceafc72e134b93a5934a045dee48f5509
Dmitry Loktevich <loktevich@gmail.com>
2019-02-03 00:51:11 +0300

    Create component for adding documents

---
+++ docs-spa/src/app/doc-add/doc-add.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DocAddComponent } from './doc-add.component';
+
+describe('DocAddComponent', () => {
+  let component: DocAddComponent;
+  let fixture: ComponentFixture<DocAddComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DocAddComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DocAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
