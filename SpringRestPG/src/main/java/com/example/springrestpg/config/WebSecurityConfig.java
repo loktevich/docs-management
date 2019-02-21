ce0b0713d272dc21cbdbb443a61537bcafc204a7
Dmitry Loktevich <loktevich@gmail.com>
2019-02-21 21:06:05 +0300

    Add security configuration and CORS filter

--- SpringRestPG/src/main/java/com/example/springrestpg/config/WebSecurityConfig.java
+++ SpringRestPG/src/main/java/com/example/springrestpg/config/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 /**
  * @author Dmitry Loktevich
@@ -31,6 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
         http.authorizeRequests()
             .anyRequest()
             .authenticated()
